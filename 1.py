ba391d82bf781aabe014dc24a762494761e92950
Sagar R <sagar181721@gmail.com>
2026-07-06 15:00:27 +0530

    .

---
+++ 1.py
@@ -0,0 +1,17 @@
+username = "sagar"
+followers = 1000
+verified = True
+rating = 4.5
+print(username)
+print(followers)
+print(verified) 
+print(rating)
+
+username = input("Enter your name:")
+followers = int(input("Enter you followeres:"))
+verified = input("veri=fied true/False:") == "True"
+rating = float(input("Enter your rating:")) 
+print("username", username)
+print("followers", followers)
+print("verified", verified)
+print("rating", rating)
