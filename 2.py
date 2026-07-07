0900f98d00cfe3df5f8e84d80eb8382df255d082
Sagar R <sagar181721@gmail.com>
2026-07-07 09:28:34 +0530

    git

---
+++ 2.py
@@ -0,0 +1,25 @@
+prodduct_price=5000
+delivery_charge=100
+total_price=prodduct_price+delivery_charge
+print(total_price)
+
+#####
+a = 10
+b = 3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#####
+a = 10
+b = 20
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
